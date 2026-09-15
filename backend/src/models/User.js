020f32b8b9b4704db8d0a867d4b2cb6c581ab285
Roshan ebi <roshanebi005@gmail.com>
2026-09-15 22:12:42 +0530

    Fix Mongoose async pre-save hook to safely handle next callback

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -69,10 +69,15 @@ const userSchema = new mongoose.Schema(
 );
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+  if (!this.isModified('password')) {
+    if (typeof next === 'function') return next();
+    return;
+  }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-  next();
+  if (typeof next === 'function') {
+    next();
+  }
 });
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
